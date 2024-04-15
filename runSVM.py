1c412ec13ae27790f250be581da298e7a296420d
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-04-15 12:28:51 -0500

    Create runSVM.py

---
+++ runSVM.py
@@ -0,0 +1,32 @@
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# reading data
+df = pd.read_csv("mouse_cortical_layers.csv")
+
+# scaling data as needed
+scaler = MinMaxScaler ()
+for column in df.columns:
+  try:
+    df[column] = scaler.fit_transform(df[[column]])
+  except:
+    df[column] = df[[column]]
+
+# extract the target variable and the feature variables
+y = df['cortical_layer_class_label']
+X = df.drop(columns = ['cell', 'cortical_layer_class_label'])
+
+# splitting into training/test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# training and testing tuned model with best hyperparameters
+bestModel = svm.SVC(kernel = 'linear', C = 0.1)
+bestModel.fit(X_train, y_train)
+y_pred = bestModel.predict(X_test)
+f1 = f1_score(y_test, y_pred, average = 'macro')
+
