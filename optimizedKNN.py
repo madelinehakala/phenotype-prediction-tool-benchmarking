a19e96a22ec9922b1fb723fb95fe83725a654a29
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-04-21 18:07:07 -0500

    Updating scripts

--- optimizedKNN.py
+++ optimizedKNN.py
@@ -8,20 +8,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("mouse_cortical_layers.csv")
 
-# checking the class balance 
-print(df['cortical_layer_class_label'].value_counts())
-
-# plotting class (im)balance
-labels = sorted(df['cortical_layer_class_label'].unique())
-counts = df['cortical_layer_class_label'].value_counts()
-plt.bar(labels, counts)
-x_labs = ['1', '2/3', '4', '5', '6']
-plt.xticks(labels, x_labs)
-plt.xlabel('Cortical Layer')
-plt.ylabel('Count')
-plt.title('Class Balance of Target Variable (Cortical Layer)')
-plt.show()
-
 # scaling data as needed
 scaler = MinMaxScaler ()
 for column in df.columns:
@@ -42,4 +28,3 @@ tunedModel = KNeighborsClassifier(leaf_size = 1)
 tunedModel.fit(X_train, y_train)
 y_pred_tuned = tunedModel.predict(X_test)
 f1_tuned = f1_score(y_test, y_pred_tuned, average = 'macro')
-print(f'F1 score: {f1_tuned}')
